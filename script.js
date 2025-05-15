499784499a16bd120426553a506cc4be85cff595
Jambi-stew <stewart.jamie122@gmail.com>
2025-05-15 11:58:28 +0100

    Create script.js
    testing javascript

---
+++ script.js
@@ -0,0 +1,64 @@
+// //PROMPTS USER TO ENTER INPUT
+// let userPrompt = prompt("Choose a number!!!");
+// console.log("userPrompt= " + userPrompt); //SHOWS INFO FROM PROMPT
+// console.log("userPrompt type = " + typeof userPrompt); //SHOWS TYPE 
+
+//const remainder = 101 % 2;
+
+//console.log(remainder);
+
+//MATHS OBJECT
+// let useMath = Math.ceil(4.2);//rounds up to 5
+// console.log(useMath);
+// let useMath = Math.floor(4.2)//rounds down to 4
+// console.log(useMath);
+// let useMath = Math.round(4.2)//rounds number accurately
+// console.log(useMath);
+//let randomNum = Math.random()*20 +1; //will set a number between 0-19 with +1 changes to 1-20
+//console.log(randomNum);
+//let randomFloor = Math.floor(randomNum);
+//console.log(randomNum)
+//console.log(randomFloor)
+
+// console.log ("hello world");
+
+// // \n INSERTS LINE BREAK IN JAVA 
+// const multiLineSentence = "this is a \n sentence";
+// const insertedVariable = "INSERTED VARIABLE";
+
+// //BEST PRACTICE. BETTER FOR FORMATTING.
+// const variableSentence = `This is an ${insertedVariable}`;
+
+// //NOT BEST PRACTICE
+// console.log ("this is an " + insertedVariable);
+// console.log (multiLineSentence);
+// console.log (variableSentence.length);
+
+// // LET NOT VAR
+// //Can turn user input into one case. 
+// let userInput = "hEllo WoRLD";
+// let lowerCase = userInput.toLowerCase();
+// console.log (lowerCase);
+
+// //String manipulation. Values start at 0 within string. Below shows how to find string lenght. How to search strings.
+
+// let fullSentence = "This is a sentence for doing Things like this string manipulation i think.";
+// console.log(fullSentence[0].toLowerCase()); //shows first letter in lowercase
+// fullSentence = fullSentence.toLowerCase(); //sets fullSentence to lowercase to allow it to be found below 
+
+// let includesThings = fullSentence.includes("things"); //searching string for set word
+// console.log(includesThings)
+
+// let lenghtSentence = fullSentence.length;
+// let lenghtSentence2 = fullSentence.length-1;
+// console.log(lenghtSentence)
+// console.log(lenghtSentence2)
+
+// let checkWord = "thing"
+// let findThing = fullSentence.indexOf(checkWord)
+// console.log(findThing)
+
+// let getThingOut = fullSentence.slice(29,34);
+// console.log (getThingOut)
+// //Replacing things
+// let replaceThing = fullSentence.replace("thing","thang")
